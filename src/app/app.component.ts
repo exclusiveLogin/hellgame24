b2f7dfe34205f54827ce0f9e76b0e5c17ea47f29
ssavin <ssavin@solit-clouds.ru>
2018-05-21 21:35:11 +0300

    adaptive menu toggler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,11 @@ export class AppComponent implements OnInit {
     private tes: TopeventsService
   ) {}
   public hidemenu = false;
+  public hidemenuToggler = false;
   @HostListener('document:scroll')
   private winScroll(): void {
     // избегаем не нужного перезаписывания и обновления события
-    if (this.hidemenu !== (document.documentElement.scrollTop > 50)) {
+    if (this.hidemenuToggler && this.hidemenu !== (document.documentElement.scrollTop > 50)) {
       this.hidemenu = (document.documentElement.scrollTop > 50);
       setTimeout(() => {
         this.tes.setMenuState(this.hidemenu);
@@ -25,6 +26,13 @@ export class AppComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    //this.auth.login({login: 'ssv', password: 'ddd'});
+    // this.auth.login({login: 'ssv', password: 'ddd'});
+    if (window && window.matchMedia) {
+      window.matchMedia('(max-width: 470px)')
+        .addListener((match: MediaQueryList) => {
+          this.hidemenuToggler = match.matches;
+        });
+      this.hidemenuToggler = window.matchMedia('(max-width: 470px)').matches;
+    }
   }
 }
