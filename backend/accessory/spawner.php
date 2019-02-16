db911a6e96873ee069c6f269c771b0b6958ec59d
Sergey Savin <savin@big3.ru>
2019-02-16 18:54:54 +0300

    spawner script single mode enabled

--- backend/accessory/spawner.php
+++ backend/accessory/spawner.php
@@ -4,6 +4,22 @@ include_once "../dbsetting_n_connect.php";
 
 $arr = new stdClass();
 
+function createRGO( $_object_id ){
+  //создаем новый RGO
+  $query = "INSERT INTO `real_game_objects` (`object_id`) VALUES ( $_object_id )";
+  //Берем последний id созданного
+  $query2 = "SELECT LAST_INSERT_ID() FROM `real_game_objects` ";
+
+  global $mysql;
+  $mysql->query($query);
+  $res = $mysql->query($query2);
+  $row = $res->fetch_row();
+  $rgo = $row[0]; // id нового созданного RGO
+
+  echo "rgo created: $rgo from object type: $_object_id > ";
+  return $rgo;
+}
+
 function createNewSlot(){
   $query = "INSERT INTO `object_slots` (`owner_type`) VALUES ( \"map\" )";
   //Берем последний id созданного
@@ -21,6 +37,7 @@ function createNewSlot(){
 
 function linkSlotOnRGO( $idSlot, $idRGO ){
 
+  echo "link Slot $idSlot and RGO $idRGO > ";
   //удаляем все линки на RGO  из  слотов
   $query = "UPDATE `object_slots` SET `rgo_id` = NULL WHERE `rgo_id`= $idRGO";
   //выставляем правильный линк с нужного слота
@@ -32,23 +49,58 @@ function linkSlotOnRGO( $idSlot, $idRGO ){
 
 }
 
+function linkSlotOnSpawn( $idSpawn, $idSlot ){
+
+  //удаляем все линки на RGO  из  слотов
+  $query = "UPDATE `object_spawn` SET `armed_slot_id` = NULL WHERE `armed_slot_id`= $idSlot";
+  //выставляем правильный линк с нужного слота
+  $query2 = "UPDATE `object_spawn` SET `armed_slot_id` = $idSlot WHERE `id`= $idSpawn";
+  //обновление last emit;
+  $query3 = "UPDATE `object_spawn` SET `last_emit` = NOW() WHERE `id`= $idSpawn";
+
+  global $mysql;
+  $mysql->query( $query );
+  $mysql->query( $query2 );
+  $mysql->query( $query3 );
+}
+
+function getFreeSpawn(){
+  $query = "SELECT * FROM `object_spawn` WHERE `armed_slot_id` IS NULL LIMIT 1";
+
+  global $mysql;
+  $res = $mysql->query( $query );
+  $row = $res->fetch_assoc();
+
+  echo "free spawn selected: $row[id] > ";
+  return $row;
+}
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-  if(isset($_GET['mode']) && isset($_GET['spawn_id']) && $_GET['mode'] == 'single'){
-    $id = $_GET['spawn_id'];
+  if(isset($_GET['mode']) && $_GET['mode'] === 'single'){
 
     // Выбор спауна
+    $spawn = getFreeSpawn();
 
     // проверка наличия
+    if( $spawn && $spawn['object_id']){
+      // создание RGO
+      $rgoID = createRGO( $spawn['object_id'] );
 
-    // создание RGO
+      // создание слота
+      $slotID = createNewSlot();
+      linkSlotOnRGO( $slotID, $rgoID );
 
-    // создание слота
+      // связывание spawn с новым слотом
+      linkSlotOnSpawn( $spawn['id'], $slotID );
 
-    // оборачивание RGO в слот
+      echo "spawn selected $spawn[id] create object type: $spawn[object_id] new slot: $slotID new RGO: $rgoID";
+    } else {
+      echo "no empty spawn";
+    }
 
-    // связывание spawn с новым слотом
+
   }
 
 }
