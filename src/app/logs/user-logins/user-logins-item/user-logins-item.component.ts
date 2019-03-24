fa6e4208c66d045fd05445a1d62cb38e2e1c7bfe
Sergey Savin <alter4444@gmail.com>
2019-03-24 16:35:30 +0300

    logins log address from dadata

--- src/app/logs/user-logins/user-logins-item/user-logins-item.component.ts
+++ src/app/logs/user-logins/user-logins-item/user-logins-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { ILoginLog } from '../../../models/user-interface';
+import { DadataService } from '../../../services/dadata.service';
+
+const ua = require('ua-parser-js');
 
 @Component({
   selector: 'app-user-logins-item-log',
@@ -26,10 +29,37 @@ export class UserLoginsItemComponent implements OnInit {
   };
 
   public _iconDeleteBtn = '<i class="fas fa-trash"></i>';
+
+  public _address: string = null;
+
+  public getUASystem(u: string){
+    let userAgent = ua(u);
+    return `${userAgent.os.name ? userAgent.os.name :''} ${userAgent.os.version ? userAgent.os.version :''}`;
+  }
+
+  public getUABrowser(u: string){
+    let userAgent = ua(u);
+    return `${userAgent.browser.name ? userAgent.browser.name :''} ${userAgent.browser.version ? userAgent.browser.version :''}`;
+  }
+
+  public getUADevice(u: string){
+    let userAgent = ua(u);
+    return `${userAgent.device.model ? userAgent.device.model :''} ${userAgent.device.type ? userAgent.device.type :''} ${userAgent.device.vendor ? userAgent.device.vendor :''}`;
+  } 
+
+  public getUADeviceType(u: string){
+    let userAgent = ua(u);
+    return userAgent.device.type ? userAgent.device.type : null;
+  } 
+
   
-  constructor() { }
+  constructor(
+    private dadata: DadataService
+  ) { }
 
   ngOnInit() {
+    if(this.item.position_lat && this.item.position_lon ) this.dadata.getAddressesFromLocation( this.item.position_lat, this.item.position_lon, this.item.accuracy )
+      .subscribe( address => this._address = address );
   }
 
   public removeThisMessage(){
