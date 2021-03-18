f794e1a88f59b781e11cce831c55a2e682c6efb6
exclucivelogin <alter4444@gmail.com>
2021-03-19 03:00:35 +0400

    remove backend bot and backup

--- backend/headers.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: content-type");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Request-Method: POST, GET, OPTION");
